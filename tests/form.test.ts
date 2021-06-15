5498c04c8c5a5a6dd17ff11f08541356fcb40ad4
Heet Vakharia <heetkv@gmail.com>
2021-06-16 01:52:46 +0530

    Updated Tests

--- tests/form.test.ts
+++ tests/form.test.ts
@@ -1,103 +1,39 @@
-const verifier = require("./../dist/index.js");
+const Verifier = require("./../dist/index.js");
 test("isUsername", () => {
-  expect(verifier.isUsername("username")).toBe(true);
-  expect(verifier.isUsername("$username")).toBe(false);
-  expect(verifier.isUsername("username", /\w{1,}/)).toBe(true);
+  expect(new Verifier("username").isUsername().correct).toBe(true);
+  expect(new Verifier("$username").isUsername().correct).toBe(false);
+  expect(new Verifier("username").isUsername(/\w{1,}/).correct).toBe(true);
 }, 500);
 test("isPassword", () => {
-  expect(verifier.isPassword("secret")).toBe(false);
-  expect(verifier.isPassword("secreT@123")).toBe(true);
-  expect(verifier.isPassword("secret", /.{1,}/)).toBe(true);
+  expect(new Verifier("secret").isPassword().correct).toBe(false);
+  expect(new Verifier("secreT@123").isPassword().correct).toBe(true);
+  expect(new Verifier("secret").isPassword(/.{1,}/).correct).toBe(true);
 });
-test("isUsernameRT", () => {
-  expect(verifier.isUsernameRT("u$sername")).toStrictEqual({
-    start: false,
-    syntax: true,
-  });
-  expect(verifier.isUsernameRT("223sername")).toStrictEqual({
-    start: true,
-    syntax: false,
-  });
-
-  expect(
-    verifier.isUsernameRT("username", {
-      length: /.{4,}/,
-      start: /^[a-zA-Z]{1,}/,
-    })
-  ).toStrictEqual({ length: false, start: false });
-}, 500);
-test("isPasswordRT", () => {
-  expect(verifier.isPasswordRT("hello")).toStrictEqual({
-    length: true,
-    lowercase: false,
-    uppercase: true,
-    symbol: true,
-  });
-  expect(verifier.isPasswordRT("secreT@123")).toStrictEqual({
-    length: false,
-    lowercase: false,
-    uppercase: false,
-    symbol: false,
-  });
-  expect(verifier.isPasswordRT("secreT", { length: /\w{1,}/ })).toStrictEqual({
-    length: false,
-  });
-}, 500);
 test("isEmail", () => {
-  expect(verifier.isEmail("wrongEmail")).toBe(false);
-  expect(verifier.isEmail("example@domain.com")).toBe(true);
+  expect(new Verifier("wrongEmail").isEmail().correct).toBe(false);
+  expect(new Verifier("example@domain.com").isEmail().correct).toBe(true);
+});
+test("Chain", () => {
+  expect(
+    new Verifier("Hello").isUsername().isLengthen("gt4").getDetails()
+  ).toStrictEqual({ length: true, start: true, syntax: true });
+  expect(
+    new Verifier("Hello").isUsername().isLengthen("gt4").isCorrect(true)
+  ).toStrictEqual(1);
+  expect(
+    new Verifier("Hello").isUsername().isLengthen("gt4").array()
+  ).toStrictEqual([
+    ["start", "syntax", "length"],
+    [true, true, true],
+  ]);
 });
 test("isLengthen", () => {
-  expect(verifier.isLengthen("hello", 5)).toBe(true);
-  expect(verifier.isLengthen("lt15", "lt5")).toBe(true);
-  expect(verifier.isLengthen("greaterthan", "gt14")).toBe(false);
-  expect(verifier.isLengthen("both", "gt4 lt10")).toBe(false);
+  expect(new Verifier("hello").isLengthen(5).correct).toBe(true);
+  expect(new Verifier("lt15").isLengthen("lt5").correct).toBe(true);
+  expect(new Verifier("greaterthan").isLengthen("gt14").correct).toBe(false);
+  expect(new Verifier("both").isLengthen("gt4 lt10").correct).toBe(false);
 });
 test("ageCalc", () => {
-  expect(verifier.ageCalc("2005-02-22")).toBe(16);
-  expect(verifier.ageCalc("2000-02-22")).toBe(21);
+  expect(new Verifier("2005-02-22").ageCalc()).toBe(16);
+  expect(new Verifier("2000-02-22").ageCalc()).toBe(21);
 }, 500);
-test("isAgeValid", () => {
-  expect(verifier.isAgeValid("2005-02-22")).toBe(false);
-  expect(verifier.isAgeValid("2000-02-22", 21)).toBe(true);
-});
-test("isCustom", () => {
-  const { includes } = verifier;
-  expect(
-    verifier.custom(
-      "hello@ga.com",
-      {
-        length: "gt2",
-        local: {
-          vstr: includes("a-zA-Z0-9"),
-          length: "gt1 lt7",
-        },
-        domain: includes("a-zA-Z"),
-        address: {
-          vstr: includes("a-zA-Z0"),
-          length: "lt10",
-          continues: true,
-        },
-      },
-      "{local}@{domain}.{address}"
-    )
-  ).toStrictEqual({
-    length: false,
-    vstr: false,
-  });
-  expect(
-    verifier.custom("hello", {
-      length: 5,
-      start: "he",
-      end: "wrong",
-      includes: includes("heslaja"),
-      excludes: "wholeHello",
-    })
-  ).toStrictEqual({
-    length: false,
-    includes: false,
-    excludes: false,
-    start: false,
-    end: true,
-  });
-});
